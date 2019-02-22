75cebb20cbbd852a11a59c9890d4c21ca3096a1c
Advance Ngwarati <advance.d.ngwarati@gmail.com>
2019-02-22 20:04:16 +0200

    Changing DB name to hesipromo

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://Hesi:h3sipromo*@cluster0-h3efu.mongodb.net/test?retryWrites=true',
-  secretOrKey: 'secret'
+  mongoURI:
+    "mongodb+srv://Hesi:h3sipromo*@cluster0-h3efu.mongodb.net/hesipromo?retryWrites=true",
+  secretOrKey: "secret"
 };
-
-
