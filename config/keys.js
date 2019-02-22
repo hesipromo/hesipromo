338b5b5b194e0faef86cf56839e29070b09bbbc0
Adonis <adonis.rumbwere@gmail.com>
2019-02-22 19:34:26 +0200

    database config done

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://Hesi:h3sipromo*@cluster0-h3efu.mongodb.net/test?retryWrites=true',
+  secretOrKey: 'secret'
+};
+
+
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+  const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
