3ec3253515495f15d87b52ffa1117e269302c662
Adonis <adonis.rumbwere@gmail.com>
2019-02-23 12:58:44 +0200

    fixing mongolab connecion bug

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+.connect(db)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
 
-  const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
