37051823cae08b8ae6bf9438e710f3f243f46e8b
Adonis <adonis.rumbwere@gmail.com>
2019-02-23 13:40:44 +0200

    local db keys

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-// Node Server Initialization
 const app = express();
 
 // DB Config
@@ -9,10 +8,10 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-.connect(db)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
