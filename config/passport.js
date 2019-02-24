b1e22eb1126e3442beb4e6fddfae61e185fa17bc
Adonis <adonis.rumbwere@gmail.com>
2019-02-24 02:03:02 +0200

    company login router fixed

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const Client = mongoose.model('clients');
+const Company = mongoose.model('companies');
 const keys = require('../config/keys');
 
 const opts = {};
--- routes/api/company.js
+++ routes/api/company.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 //Load Company Model
 const Company = require('../../models/Company');
@@ -48,5 +51,67 @@ router.post('/register', (req, res) =>{
     })
 
 });
+
+//@route   POST api/company/login
+//@desc    Login company / Returning a Token
+//@access  Public
+router.post('/login', (req, res) => {
+const email = req.body.email;
+const password = req.body.password;
+
+//Find Company by email
+Company.findOne({email})
+.then( company => {
+    //Check for Company
+if(!company){
+    return res.json.status(404)({email: 'Company email not found'})
+}
+
+//Check for Password
+bcrypt.compare(password, company.password)
+.then(isMatch => {
+    if(isMatch){
+        //company Matched
+        const payload = { id: company.id, name: company.name}; // Create JWT Payload
+        //Sign Token
+        jwt.sign(
+            payload,
+            keys.secretOrKey,
+            {expiresIn: 3600},
+            (err, token) => {
+                res.json({
+                    success: true,
+                    token: 'Bearer ' + token
+                });
+
+            });
+
+    } else {
+        return res.status(400).json({password: "Password Incorrect"});
+    }
+
+});
+
+
+});
+
+});
+
+//@route   GET api/company/current
+//@desc    Return current company
+//@access  Private
+
+router.get(
+    '/current',
+     passport.authenticate('jwt', { session: false}),
+ (req, res) => {
+    res.json({
+        id: req.company.id,
+        name: req.company.name,
+        email: req.company.email
+    });
+});
+
+
 
 module.exports = router;
