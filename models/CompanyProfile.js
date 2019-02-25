3270d6974a9382a55648154974c92b95eadb4aeb
Adonis <adonis.rumbwere@gmail.com>
2019-02-25 03:26:01 +0200

    cannot find node module error fixed

---
+++ models/CompanyProfile.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Creating a Schema
+const CompanyProfileSchema = new Schema({
+  company: {
+    type: Schema.Types.ObjectId,
+    ref: 'company'
+  },
+  name:{
+    type: String,
+    required: true
+  } 
+});
+
+module.exports = CompanyProfile = mongoose.model('companyprofile', CompanyProfileSchema);
