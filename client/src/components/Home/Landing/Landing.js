08e34761eea8970b44183c66ae5c42facc785baa
Advance Ngwarati <advance.d.ngwarati@gmail.com>
2019-04-22 15:02:38 +0200

    landing fix

--- client/src/components/Home/Landing/Landing.js
+++ client/src/components/Home/Landing/Landing.js
@@ -20,8 +20,12 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-xl-9 mx-auto">
-                <h1 className="mb-5">Build a landing page for your business or project and generate more leads!</h1>
+                <h1 className="mb-5">Never miss a promotion, discount from your favourite Stores!</h1>
               </div>
+              <div className="col-xl-6 mx-auto">
+                <h3 className="mb-5">Food, drinks and grcoeries promos up for grabs.</h3>
+              </div>
+              
               <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
                 <form>
                   <div className="form-row">
@@ -29,7 +33,7 @@ class Landing extends Component {
                       <input type="email" className="form-control form-control-lg" placeholder="Enter your city"/>
                     </div>
                     <div className="col-12 col-md-3">
-                      <button type="submit" className="btn btn-block btn-lg">Find Promo</button>
+                      <button type="submit" className="btn btn-block btn-lg">Find Promos</button>
                     </div>
                   </div>
                 </form>
